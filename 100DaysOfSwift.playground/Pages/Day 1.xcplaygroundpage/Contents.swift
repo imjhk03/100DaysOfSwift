4ef383646056cab8e7816b5c0a035cd37aab17a7
Joohee Kim <imjhk03@gmail.com>
2024-01-19 11:47:14 +0900

    Xcode Playground multiple page 생성

--- 100DaysOfSwift.playground/Pages/Day 1.xcplaygroundpage/Contents.swift
+++ 100DaysOfSwift.playground/Pages/Day 1.xcplaygroundpage/Contents.swift
@@ -1,3 +1,8 @@
+/*:
+ [< Previous](@previous)
+ # Day 1 - variables, simple data types, and string interpolation
+ */
+
 import UIKit
 
 // Variables
@@ -40,11 +45,13 @@ let year: Int = 1989
 let height: Double = 1.78
 let taylorRocks: Bool = true
 
-// Summary
-/*
+/*:
+ ## Summary
  1. You make variables using var and constants using let. It’s preferable to use constants as often as possible.
  2. Strings start and end with double quotes, but if you want them to run across multiple lines you should use three sets of double quotes.
  3. Integers hold whole numbers, doubles hold fractional numbers, and booleans hold true or false.
  4. String interpolation allows you to create strings from other variables and constants, placing their values inside your string.
  5. Swift uses type inference to assign each variable or constant a type, but you can provide explicit types if you want.
  */
+
+//: [Next](@next)
---
+++ 100DaysOfSwift.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,6 @@
+/*:
+ # 100 Days of Swift
+ ## Table of Contents
+ 1. [Day 1 - variables, simple data types, and string interpolation](Day%201)
+ 2. [Day 2 – arrays, dictionaries, sets, and enums](Day%202)
+ */
