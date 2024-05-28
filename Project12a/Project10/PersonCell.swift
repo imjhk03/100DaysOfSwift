39622ab129b9194b16f7bfee22b70b539f3ddb36
Joohee Kim <imjhk03@gmail.com>
2024-05-28 21:32:30 +0900

    프로젝트12a 수정

---
+++ Project12a/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Joohee Kim on 5/27/24.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
