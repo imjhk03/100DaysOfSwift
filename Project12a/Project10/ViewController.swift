5c0c4894380e06e6ecf2010586668b6750b65704
Joohee Kim <imjhk03@gmail.com>
2024-05-28 17:02:54 +0900

    Day 48 – Project 12, part one

--- Project12a/Project10/ViewController.swift
+++ Project12a/Project10/ViewController.swift
@@ -14,14 +14,6 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         super.viewDidLoad()
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
-        
-        let defaults = UserDefaults.standard
-        
-        if let savedPeople = defaults.object(forKey: "people") as? Data {
-            if let decodedPeople = try? NSKeyedUnarchiver.unarchiveObject(with: savedPeople) as? [Person] {
-                people = decodedPeople
-            }
-        }
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -81,7 +73,6 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         
         let person = Person(name: "Unknown", image: imageName)
         people.append(person)
-        save()
         collectionView.reloadData()
         
         dismiss(animated: true)
@@ -115,7 +106,6 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] _ in
             guard let newName = ac?.textFields?[0].text else { return }
             person.name = newName
-            self?.save()
             self?.collectionView.reloadData()
         })
         
@@ -133,13 +123,6 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         present(ac, animated: true)
-    }
-    
-    func save() {
-        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: people, requiringSecureCoding: false) {
-            let defaults = UserDefaults.standard
-            defaults.set(savedData, forKey: "people")
-        }
     }
 }
 
